6cd861effc1a38df4a7fb759ded3d92f694a6d7c
StephenBrink <Blackxcap@gmail.com>
2022-01-10 00:53:57 -0600

    Added a button and move App.js around, image isn't loading but that is a problem for tomorrow

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 //import logo from './logo.svg';
 import React, {Component, Fragment} from 'react';
+import { Button, TouchableOpacity, Text, Image, StyleSheet } from 'react-native';
 import {Route} from 'react-router-dom';
 import withSplashScreen from './components/splash-screen';
 
 import './App.css';
 
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#859a9b',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },
+});
+
 class App extends Component {
   render () {
     return (
-      getHomeScreen()
+      <div className="App">
+        <header className="App-header">
+          {getHomeScreen()}
+          {getSoundboardButtons()}
+        </header>
+      </div>
     );
   }
 }
 
+function getSoundboardButtons(props) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={() => console.log('Button was pressed')}>
+      <Image source={require("./assets/cat.png")}/>
+    </TouchableOpacity>
+  );
+}
+
 function getHomeScreen() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          I have loaded
-        </p>
-      </header>
-    </div>
+    <p> I have loaded </p>
   );
 }
 
